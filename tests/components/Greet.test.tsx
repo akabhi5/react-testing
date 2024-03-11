857c1636c1a480b2f59ad79c71a624d299edbfd0
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-11 23:32:08 +0530

    Greet - component test

---
+++ tests/components/Greet.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import Greet from "../../src/components/Greet";
+import "@testing-library/jest-dom/vitest";
+
+describe("Greet", () => {
+  it("should render Hello with the name when name is provided", () => {
+    render(<Greet name="Abhi" />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/abhi/i);
+  });
+
+  it("should render login button when name is not provided", () => {
+    render(<Greet />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
