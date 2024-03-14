69508efe70a2c293566fb572b918f903024e122b
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-14 22:58:47 +0530

    UserList, ProductImageGallery

---
+++ tests/components/UserList.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import UserList from "../../src/components/UserList";
+import { User } from "../../src/entities";
+
+describe("UserList", () => {
+  it("should render no users when users array is empty", () => {
+    render(<UserList users={[]} />);
+    expect(screen.getByText(/no users/i)).toBeInTheDocument();
+  });
+
+  it("should render the list of users", () => {
+    const users: User[] = [
+      { id: 1, name: "Abhi" },
+      { id: 2, name: "Sabhi" },
+    ];
+    render(<UserList users={users} />);
+    users.forEach((user) => {
+      const link = screen.getByRole("link", { name: user.name });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", `/users/${user.id}`);
+    });
+  });
+});
