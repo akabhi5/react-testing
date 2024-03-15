be6969fbd0ecb4ae28f5e14789ea269a81b08eb3
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-16 00:24:51 +0530

    TagList

---
+++ tests/components/TagList.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { it, expect, describe } from "vitest";
+import TagList from "../../src/components/TagList";
+
+describe("TagList", () => {
+  it("should render tags", async () => {
+    render(<TagList />);
+
+    // this checks for content after certain period of time till timeout
+    // await waitFor(() => {
+    //   const listItems = screen.getAllByRole("listitem");
+    //   expect(listItems.length).toBeGreaterThan(0);
+    // });
+
+    const listItems = await screen.findAllByRole("listitem");
+    expect(listItems.length).toBeGreaterThan(0);
+  });
+});
