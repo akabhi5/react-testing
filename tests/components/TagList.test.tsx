13ae09d45911a1cd7397f0c83571da005760e9fc
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-16 21:37:10 +0530

    ToastDemo

--- tests/components/TagList.test.tsx
+++ tests/components/TagList.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { it, expect, describe } from "vitest";
 import TagList from "../../src/components/TagList";
 
