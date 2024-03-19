82fa4b704ca9165d2f8c1acc91df4af0c569ee4a
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-19 23:52:34 +0530

    ProductForm with product

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -2,8 +2,20 @@ import { it, expect, describe } from "vitest";
 import ProductForm from "../../src/components/ProductForm";
 import { render, screen } from "@testing-library/react";
 import AllProviders from "../AllProviders";
+import { Category, Product } from "../../src/entities";
+import { db } from "../mocks/db";
 
 describe("ProductForm", () => {
+  let category: Category;
+
+  beforeAll(() => {
+    category = db.category.create();
+  });
+
+  afterAll(() => {
+    db.category.delete({ where: { id: { equals: category.id } } });
+  });
+
   it("should render form fields", async () => {
     render(<ProductForm onSubmit={vi.fn()} />, {
       wrapper: AllProviders,
@@ -16,5 +28,28 @@ describe("ProductForm", () => {
     expect(
       screen.getByRole("combobox", { name: /category/i })
     ).toBeInTheDocument();
+  });
+
+  it("should render form fields when editing a product", async () => {
+    const product: Product = {
+      id: 1,
+      name: "Bread",
+      price: 10,
+      categoryId: category.id,
+    };
+
+    render(<ProductForm product={product} onSubmit={vi.fn()} />, {
+      wrapper: AllProviders,
+    });
+
+    await screen.findByRole("form");
+
+    expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
+    expect(screen.getByPlaceholderText(/price/i)).toHaveValue(
+      product.price.toString()
+    );
+    expect(
+      screen.getByRole("combobox", { name: /category/i })
+    ).toHaveTextContent(category.name);
   });
 });
