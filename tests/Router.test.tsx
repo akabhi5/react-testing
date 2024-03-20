c8928b7d863b0bbbe7c6aac7bc3a1a86636c3a60
Abhishek Kumar <abhishekk580@gmail.com>
2024-03-20 14:26:27 +0530

    product detail

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import { navigateTo } from "./utils";
+import { db } from "./mocks/db";
 
 describe("Router", () => {
   it("should render the home page for /", () => {
@@ -15,5 +16,17 @@ describe("Router", () => {
     expect(
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
+  });
+
+  it("should render the product details page for /products/:id", async () => {
+    const product = db.product.create();
+
+    navigateTo("/products/" + product.id);
+
+    expect(
+      await screen.findByRole("heading", { name: product.name })
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
   });
 });
